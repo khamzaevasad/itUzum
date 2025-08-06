4ae9c40490aac23563d4f31892bb924f013a60f1
khamzaevasad <xamzayevasad442@gmail.com>
2025-08-07 08:19:34 +0900

    "fix: correct discount calculation and prevent image index errors in carousel"

--- js/updataUi.js
+++ js/updataUi.js
@@ -43,7 +43,46 @@ export const updateUI = (data) => {
 };
 
 // product update
+const brandName = document.getElementById("brand-name");
+const productCategory = document.getElementById("product-category");
+const productTitle = document.getElementById("product-title");
+const productInfoDiscount = document.getElementById("product-info-discount");
+const productInfoPrice = document.getElementById("product-info-price");
+const discountInfoPrice = document.getElementById("discount-info-price");
+const shippingInfo = document.getElementById("shipping-info");
+const minimumOrder = document.getElementById("minimum-order");
+const availability = document.getElementById("availability");
+const warranty = document.getElementById("warranty");
 
 export const updateProduct = (product) => {
   console.log(product);
+
+  const imagesContainer = document.getElementById("carousel-container");
+
+  imagesContainer.innerHTML = "";
+
+  product.images.forEach((imgUrl) => {
+    const div = document.createElement("div");
+    div.className = "carousel-item w-full";
+
+    const img = document.createElement("img");
+    img.src = imgUrl;
+    img.className = "w-full";
+
+    div.appendChild(img);
+    imagesContainer.appendChild(div);
+  });
+
+  brandName.textContent = product.brand;
+  productCategory.textContent = product.category;
+  productTitle.textContent = product.title;
+  productInfoDiscount.textContent = `${product.discountPercentage}%`;
+  productInfoPrice.textContent = `$${product.price}`;
+  discountInfoPrice.textContent = `$${Math.floor(
+    product.price * (1 - product.discountPercentage / 100).toFixed(2)
+  )}`;
+  shippingInfo.textContent = product.shippingInformation;
+  minimumOrder.textContent = product.minimumOrderQuantity;
+  availability.textContent = product.availabilityStatus;
+  warranty.textContent = product.warrantyInformation;
 };
